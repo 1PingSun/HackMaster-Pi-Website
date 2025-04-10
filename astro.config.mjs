4524aa9c4768965abc22d9938f1c2c4e812540d7
piinng <52sunyiping@gmail.com>
2025-04-10 11:26:39 +0800

    2025-04-10 Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,21 +6,27 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'HackMaster Pi',
+			logo: {
+				dark: './src/assets/PNG-Dark.webp',
+				light: './src/assets/PNG-Light.webp',
+				alt: 'HackMaster Pi Logo',
+				replacesTitle: true,
+			},
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/1PingSun/HackMaster-Pi' }],
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					label: 'Initial Setup',
+					autogenerate: { directory: 'initial-setup' },
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Usage',
+					autogenerate: { directory: 'usage' },
 				},
 			],
 		}),
 	],
+
+	site: 'https://hackmasterpi.org/',
+	favicon: '/favicon.ico'
 });
